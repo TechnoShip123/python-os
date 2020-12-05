58a7e85fed2449788e7f5d69ec5756a519c54a02
FlyingShip123 <75491816+TechnoShip123@users.noreply.github.com>
2020-12-05 12:15:53 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+
+username = [
+    'Jack',
+    'username2',
+    'username3'
+]
+password = {
+    'Jack': 'Jack123',
+    'username2': 'password2',
+    'username3': 'password3',
+}
+logging.basicConfig(filename='incidents.log', filemode='a', level=logging.WARN, format='%(asctime)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+
+Uinput = input('Username >> ')
+if Uinput not in username:
+    logging.error('Attempted login with invalid USERNAME')
+    exit('Invalid username. This incident will be reported.')
+
+Pinput = input('Password >> ')
+if Pinput != password[Uinput]:
+    logging.error('Attempted login with invalid PASSWORD')
+    exit('Incorrect password. This incident will be reported.')
+
+print('Valid credentials. Welcome to the system! :D')
+
+
+# TODO: Find a way to implement Hashing
