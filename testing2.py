30b9d4e518e6586340001c9a3e880d6b76ecc42b
TechnoShip123 <sopan.jalkote@gmail.com>
2020-12-20 09:33:16 -0600

    Small changes, experimenting with imports from my own created files into the `testing2.py` for the bcolors. Maybe make this a module...

--- testing2.py
+++ testing2.py
@@ -1,21 +1,11 @@
 #! bin/var/env Python
 from tkinter import *
+from betterPrint import bcolors
 import os
 
 
 # Designing window for registration
 # Self Notes: The current background color is teal, so try finishing your TODO: Find a good background color.
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 def register():
     global register_screen
